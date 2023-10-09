4770a5b6940d5f9185d6e04c57f8d1a1035def83
wwxqi <github.wwxqi@slmail.me>
2023-10-08 20:29:25 -0400

    Add function to decompress .gz files

--- src/whatsincc/file_operations.py
+++ src/whatsincc/file_operations.py
@@ -30,3 +30,24 @@ def download_file(url: str) -> None:
     print("Done.")
 
     return
+
+
+def decompress_gz(gz_path: str) -> None:
+    """
+    Decompresses .gz compressed file. As common crawl hosts only compressed files.
+    Saves decompressed file in same path as compressed file.
+    """
+    decompressed_path = os.path.splitext(gz_path)[0]
+
+    # Check if file already exists,
+    # TODO: maybe also check for matching checksum
+    if os.path.isfile(decompressed_path):
+        print("File already exists")
+        return
+
+    # unzip with gzip using
+    # https://stackoverflow.com/questions/31028815/how-to-unzip-gz-file-using-python#44712152
+    with gzip.open(gz_path, 'rb') as f_in:
+        with open(decompressed_path, 'wb') as f_out:
+            shutil.copyfileobj(f_in, f_out)
+    return
