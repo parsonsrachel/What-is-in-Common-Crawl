5dffdc6916d6d00a916f8b870d135a559a400305
wwxqi <github.wwxqi@slmail.me>
2023-10-10 21:41:47 -0400

    Adjust funtions to return path

--- src/whatsincc/file_operations.py
+++ src/whatsincc/file_operations.py
@@ -4,7 +4,7 @@ import requests
 import shutil
 
 
-def download_file(url: str) -> None:
+def download_file(url: str, decompress: bool = False) -> str:  # TODO: os.paht
     local_filename = url.split('/')[-1]  # e.g. "warc.paths.gz"
     local_file_dir = url.split('/')[-2]  # e.g. "CC-MAIN-2013-20"
     local_file_dir_path = f"../data/{local_file_dir}/"  # e.g. ./data/CC-MAIN-2013-20
@@ -13,26 +13,27 @@ def download_file(url: str) -> None:
     # Check if file already exists,
     # TODO: maybe also check for matching checksum
     if os.path.isfile(local_file_path):
-        print("File already exists")
-        return
+        if decompress:
+            return decompress_gz(local_file_path)
+        return local_file_path
 
     # create directory if it doesn't exist
     if not os.path.isdir(local_file_dir_path):
-        print(f"Creating directory {local_file_dir_path}")
         os.mkdir(local_file_dir_path)
 
     # Download file. To not use too much memory using method from
     # https://stackoverflow.com/questions/16694907/download-large-file-in-python-with-requests
-    print(f"Downloading File to {local_file_path}")
     with requests.get(url, stream=True) as r:
         with open(local_file_path, 'wb') as f:
             shutil.copyfileobj(r.raw, f)
-    print("Done.")
 
-    return
+    if decompress:
+        return decompress_gz(local_file_path)
+
+    return local_file_path
 
 
-def decompress_gz(gz_path: str) -> None:
+def decompress_gz(gz_path: str) -> os.path:  # TODO: os.pats
     """
     Decompresses .gz compressed file. As common crawl hosts only compressed files.
     Saves decompressed file in same path as compressed file.
@@ -42,12 +43,11 @@ def decompress_gz(gz_path: str) -> None:
     # Check if file already exists,
     # TODO: maybe also check for matching checksum
     if os.path.isfile(decompressed_path):
-        print("File already exists")
-        return
+        return decompressed_path
 
     # unzip with gzip using
     # https://stackoverflow.com/questions/31028815/how-to-unzip-gz-file-using-python#44712152
     with gzip.open(gz_path, 'rb') as f_in:
         with open(decompressed_path, 'wb') as f_out:
             shutil.copyfileobj(f_in, f_out)
-    return
+    return decompressed_path
