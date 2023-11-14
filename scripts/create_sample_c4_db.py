d35c208f56eae95909c0e615a545cf089f26f5fe
Alex Lee <alee01@terpmail.umd.edu>
2023-11-14 14:55:36 -0500

    Add token column to database

--- scripts/create_sample_c4_db.py
+++ scripts/create_sample_c4_db.py
@@ -1,5 +1,6 @@
 from datasets import load_dataset
 import sqlite3
+import tiktoken
 
 # Create and or connect to database
 con = sqlite3.connect("./data/c4_sample.db")
@@ -12,18 +13,24 @@ if continue_prompt != "y":
 	quit()
 
 cur.execute("DROP TABLE IF EXISTS c4_sample")
-cur.execute("CREATE TABLE IF NOT EXISTS c4_sample(url, timestamp, content)")
+cur.execute("CREATE TABLE IF NOT EXISTS c4_sample(url, timestamp, content, n_tokens)")
 con.commit()
 
 dataset = load_dataset('c4', 'en', split='train', streaming=True)
 shuffled_dataset = dataset.shuffle(buffer_size=1, seed=42)
 shuffled_dataset_iter = iter(shuffled_dataset)
 
+encoding = tiktoken.encoding_for_model('gpt-3.5-turbo')
+
 n = 10_000
 for _ in range(n):
     entry = next(shuffled_dataset_iter)
-    data = (entry['url'], entry['timestamp'], entry['text'])
-    cur.execute(f"INSERT INTO c4_sample VALUES(?,?,?)", data)
+
+    token_integers = encoding.encode(entry['text'])
+    num_tokens = len(token_integers)
+    
+    data = (entry['url'], entry['timestamp'], entry['text'], num_tokens)
+    cur.execute(f"INSERT INTO c4_sample VALUES(?,?,?,?)", data)
 con.commit()
 
 print("Database created")
