46faf332fd55f27b8d1a07be2ffa18dde3a9fbe2
Rachel Parsons <143442234+parsonsrachel@users.noreply.github.com>
2023-12-14 11:19:42 -0500

    Update test_copyright.py

--- scripts/test_copyright.py
+++ scripts/test_copyright.py
@@ -3,9 +3,14 @@ import sqlite3
 con = sqlite3.connect('c4_sample_2.db')
 cur = con.cursor()
 
+#select all content
 cur.execute("SELECT content FROM c4_sample WHERE copyrighted='1'")
 result_set = cur.fetchall()
 
 for res in result_set:
     c_symbol_set = res[0].split('©')
+    #assert that symbol was found
     assert(len(c_symbol_set) > 1)
+
+con.commit() 
+cur.close()
