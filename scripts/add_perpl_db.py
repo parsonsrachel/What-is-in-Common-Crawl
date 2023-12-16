3d2fadd7193632e22c9054fca1f29673682cadc5
Alex Lee <alee01@terpmail.umd.edu>
2023-12-16 06:21:46 -0500

    Improve comments and PEP styling

--- scripts/add_perpl_db.py
+++ scripts/add_perpl_db.py
@@ -2,6 +2,10 @@ import sqlite3
 import pyplexity
 import argparse
 
+# Calculates the perplexity score of content in the database and adds column with the perplexity score
+# The LM can be chosen: British National Corpus (BNC) or CORD-19, and either bigrams or trigrams
+
+# Takes in a model argument
 argParser = argparse.ArgumentParser()
 argParser.add_argument("-m", "--model",action='store',
                          dest='model_str',default='bigrams-bnc',
@@ -12,6 +16,7 @@ args = argParser.parse_args()
 print("Model: " + args.model_str)
 model = pyplexity.PerplexityModel.from_str(args.model_str)
 
+# Connects to the database
 con = sqlite3.connect("./data/c4_sample.db")
 cur = con.cursor()
 
@@ -22,22 +27,26 @@ if continue_prompt != "y":
 
 col_model = args.model_str.replace("-", "_")
 
+# Drop perplexity column, if it does not exists do nothing
+# Column name is the model name
 try:
     cur.execute("ALTER TABLE c4_sample DROP COLUMN " + col_model)
 except:
      print("Column does not exist")
 
+# Add perplexity column
 cur.execute("ALTER TABLE c4_sample ADD COLUMN " + col_model + " FLOAT")
 
+# Fetch all rows in database
 cur.execute("SELECT url, timestamp, content FROM c4_sample")
 rows = cur.fetchall()
 
-# Iterate through rows and update n_tokens column
+# Iterate through rows and update perplexity column
 for row in rows:
     url, timestamp, content = row
     perpl = model.compute_sentence(content)
 
-    # Update n_tokens column in the database
+    # Update perplexity column in the database
     cur.execute("UPDATE c4_sample SET " + col_model + " = ? WHERE url = ? AND timestamp = ?", (perpl, url, timestamp))
 
 # Commit the changes to the database
