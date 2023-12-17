3bd5dbd50a87a4a373c0a17b919381e56abec6bb
wwxqi <github.wwxqi@slmail.me>
2023-12-16 20:48:18 -0500

    Add test for consistency of classifier

--- tests/classifier_test.py
+++ tests/classifier_test.py
@@ -14,6 +14,18 @@ class TestGoogleClassifier(unittest.TestCase):
         classification = next(iter(google_cloud_lang.classify(text)))
         self.assertEqual(classification, "/Sports/College Sports")
 
+    def test_consistency(self):
+        text = ("I the south French countryside is the town of Camargue on the largest river delta in Europe known for \
+                its iconic pink flamingoes and salt that has been harvested since the Middle Ages. Complete with \
+                paddocks overflowing with statuesque white horses & mean looking black bulls rutting in the dust with \
+                rippling muscles bred for bull fighting. \
+                Bastille Day is the largest public celebration in France, when its citizens celebrate the \
+                anniversary of the storming of the Bastille on 14th July 1789. This was the beginning of the \
+                end of the French monarchy, launching the French Revolution.")
+
+        classification = google_cloud_lang.classify(text)
+        self.assertEqual(classification, google_cloud_lang.classify(text))
+
 
 if __name__ == '__main__':
     unittest.main()
